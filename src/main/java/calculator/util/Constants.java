ebc8a42107f10a002318551c3c5c2761b02af31d
jihoo2002 <yolohoo@naver.com>
2025-10-20 03:29:11 +0900

    feat : 계산기에서 사용하는 상수들을 모은 Constants 클래스 추가

---
+++ src/main/java/calculator/util/Constants.java
@@ -0,0 +1,9 @@
+package calculator.util;
+
+public class Constants {
+    public static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SEPARATOR = "\\n";
+    public static final String POSITIVE_NUMBER_REGEX = "\\d+";
+    public static final int CUSTOM_DELIMITER_PREFIX_LENGTH = 2;
+}

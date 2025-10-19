b4911be247b698618b6e6db6264da13b901018e1
jihoo2002 <yolohoo@naver.com>
2025-10-20 04:02:24 +0900

    feat : 계산기 애플리케이션의 실행 진입점(Main) 클래스 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import calculator.service.CalculatorService;
+import calculator.view.CalculatorView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorView view = new CalculatorView();
+        String input = view.calculatorInput();
+        CalculatorService service = new CalculatorService();
+        long calculator = service.calculate(input);
+        view.calculatorOutput(calculator);
     }
 }

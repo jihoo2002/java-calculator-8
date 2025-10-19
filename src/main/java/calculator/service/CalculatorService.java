bdf588e6409cc0739d8aebe9eb4b97a04bd12594
jihoo2002 <yolohoo@naver.com>
2025-10-20 04:06:26 +0900

    feat: CalculatorService calculate 기능 단계별 메서드로 분리

---
+++ src/main/java/calculator/service/CalculatorService.java
@@ -0,0 +1,39 @@
+package calculator.service;
+
+import calculator.domain.Calculator;
+import calculator.parser.CalculatorNumberParser;
+import calculator.splitter.CalculatorInputSplitter;
+import calculator.validator.CalculatorInputValidator;
+
+import java.util.List;
+
+public class CalculatorService {
+
+    public long calculate(String input) {
+        validateInput(input);
+        String[] numbers = splitInput(input);
+        validateNoNegative(numbers);
+        List<Long> parsedNumbers = parseNumbers(numbers);
+        return sumNumbers(parsedNumbers);
+    }
+
+    private void validateInput(String input) {
+        CalculatorInputValidator.validateInput(input);
+    }
+
+    private String[] splitInput(String input) {
+        return CalculatorInputSplitter.split(input);
+    }
+
+    private void validateNoNegative(String[] numbers) {
+        CalculatorInputValidator.validateNoNegativeNumbers(numbers);
+    }
+
+    private List<Long> parseNumbers(String[] numbers) {
+        return CalculatorNumberParser.parseNumbers(numbers);
+    }
+
+    private long sumNumbers(List<Long> numbers) {
+        return Calculator.sum(numbers);
+    }
+}
